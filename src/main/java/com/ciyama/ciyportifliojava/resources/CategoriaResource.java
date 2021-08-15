d21f42e42444b936ecfa6b021caf8266533f99fc
Celio Iyama <celio.iyama@gmail.com>
2021-08-14 22:04:45 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/ciyama/ciyportifliojava/resources/CategoriaResource.java
+++ src/main/java/com/ciyama/ciyportifliojava/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.ciyama.ciyportifliojava.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ciyama.ciyportifliojava.domain.Categoria;
+import com.ciyama.ciyportifliojava.dto.CategoriaDTO;
 import com.ciyama.ciyportifliojava.services.CategoriaService;
 
 import javassist.tools.rmi.ObjectNotFoundException;
@@ -48,4 +52,13 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() throws ObjectNotFoundException{
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	
  }
