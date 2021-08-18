3f425816303052b23298eee7c937457fd71b6edb
Celio Iyama <celio.iyama@gmail.com>
2021-08-18 10:58:00 -0300

    reorganização do gitignore

---
+++ src/main/java/com/ciyama/ciyportifliojava/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,52 @@
+package com.ciyama.ciyportifliojava.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ciyama.ciyportifliojava.domain.Cliente;
+import com.ciyama.ciyportifliojava.domain.enums.TipoCliente;
+import com.ciyama.ciyportifliojava.dto.ClienteNewDTO;
+import com.ciyama.ciyportifliojava.repositories.ClienteRepository;
+import com.ciyama.ciyportifliojava.resources.exception.FieldMessage;
+import com.ciyama.ciyportifliojava.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
+		}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já cadastrado"));
+		}
+
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
