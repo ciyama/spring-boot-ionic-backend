3dbcf486afe4e2fb5ef51a0048aa9d5412fd5bdf
Celio Iyama <celio.iyama@gmail.com>
2021-08-18 22:42:00 -0300

    Subtotal e total de Pedido

--- src/main/java/com/ciyama/ciyportifliojava/repositories/ItemPedidoRepository.java
+++ src/main/java/com/ciyama/ciyportifliojava/repositories/ItemPedidoRepository.java
@@ -1,4 +1,4 @@
-package com.ciyama.ciyportifliojava.repositories;
+ package com.ciyama.ciyportifliojava.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
